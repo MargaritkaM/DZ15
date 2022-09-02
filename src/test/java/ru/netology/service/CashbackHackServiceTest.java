a23e6997baae92c727678f21e5d8d911c959f776
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-09-02 10:03:03 +0300

    Test changed

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -30,7 +30,7 @@ class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
-        int expected = 0;
+        int expected = 1000;
 
         assertEquals(expected, actual);
     }

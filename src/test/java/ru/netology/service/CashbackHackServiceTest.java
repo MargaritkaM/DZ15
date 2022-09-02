04052ab371e4de5f3802354f7e15ca412ef000f2
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-09-02 10:54:50 +0300

    Change expected Error

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -30,7 +30,7 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
-        int expected = 1;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
